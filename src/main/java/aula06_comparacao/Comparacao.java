e21a1fc0aa3cfdd3b68e8d739c9aa889edea7d44
Leandro Guarino <le.guarino@gmail.com>
2020-09-12 15:42:59 -0300

    Código da aula

---
+++ src/main/java/aula06_comparacao/Comparacao.java
@@ -0,0 +1,50 @@
+package aula06_comparacao;
+
+import java.util.ArrayList;
+
+public class Comparacao {
+    public static void main(String[] args) {
+        
+        ListaLigada<Integer> lista = new ListaLigada<Integer>();
+        
+        ArrayList<Integer> vetor = new ArrayList<Integer>();
+        
+        //Adicionar elementos
+        int limite = 10000;
+        long tempoInicial = System.currentTimeMillis();
+        long tempoFinal;
+        for(int i=0; i < limite; i++){
+            vetor.add(i);
+        }
+        tempoFinal = System.currentTimeMillis();
+        System.out.println("Adicionou "+limite+" elementos no vetor");
+        System.out.println(tempoFinal - tempoInicial);
+        
+        tempoInicial = System.currentTimeMillis();
+        for(int i=0; i < limite; i++){
+            lista.adicionar(i);
+        }
+        tempoFinal = System.currentTimeMillis();
+        System.out.println("\n\nAdicionou "+limite+" elementos na lista");
+        System.out.println(tempoFinal - tempoInicial);
+        
+        //ler valores
+        tempoInicial = System.currentTimeMillis();
+        for(int i=0; i < vetor.size(); i++){
+            vetor.get(i);
+        }
+        tempoFinal = System.currentTimeMillis();
+        System.out.println("\n\nTempo de leitura do vetor");
+        System.out.println(tempoFinal - tempoInicial);
+        
+        tempoInicial = System.currentTimeMillis();
+        for(int i=0; i < lista.getTamanho(); i++){
+            lista.get(i);
+        }
+        tempoFinal = System.currentTimeMillis();
+        System.out.println("\n\nTempo de leitura da lista");
+        System.out.println(tempoFinal - tempoInicial);
+        
+        
+    }
+}
